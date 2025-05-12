edb60c4f1c3b4744b56334fe2f42fefa228d1627
amina-mardiyyah <amina.rufai@yahoo.com>
2025-05-12 14:28:15 +0100

    add utility scripts

---
+++ Entity-Extraction-Modular-pipeline/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Entity-Extraction-Modular-pipeline",
+    version="0.1.0",
+    packages=find_packages(),  # auto‐discovers packages
+)

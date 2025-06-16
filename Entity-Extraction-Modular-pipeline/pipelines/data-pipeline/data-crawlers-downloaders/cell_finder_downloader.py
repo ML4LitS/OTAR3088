2d2134d8e977a9514ed7f95d743ba18b934739fd
amina-mardiyyah <amina.rufai@yahoo.com>
2025-06-16 20:35:42 +0100

    update cell-finder-script

--- Entity-Extraction-Modular-pipeline/pipelines/data-pipeline/data-crawlers-downloaders/cell_finder_downloader.py
+++ Entity-Extraction-Modular-pipeline/pipelines/data-pipeline/data-crawlers-downloaders/cell_finder_downloader.py
@@ -1,20 +1,38 @@
+
+#standard
+from importlib import metadata
 import os
 import requests
-from requests import HTTPError, RequestException
 from pathlib import Path
 from bs4 import BeautifulSoup as bs
 from typing import List, Tuple
 from typing_extensions import Annotated, Optional
 import tarfile
-from utils.helper_functions import catch_request_errors
 from argparse import ArgumentParser
+
+# #orchestrator
+import wandb
+
 import sys
 from pathlib import Path
 
+# from zenml import step ###temporarily disabled
+
+#load environmental variables
+from dotenv import load_dotenv
+
+#custom
+from utils.helper_functions import catch_request_errors, create_output_dir
 
 
+#load environment variables
+load_dotenv()
+BASE_PATH = os.environ.get("BASE_PATH")
+WANDB_TOKEN = os.environ.get("WANDB_TOKEN")
 
+#CELL FINDER SOURCE URL
 BASE_URL = "https://www.informatik.hu-berlin.de/de/forschung/gebiete/wbi/resources/cellfinder/"
+
 
 
 
@@ -37,14 +55,15 @@ def download_and_extract(data_links:List[str], output_dir:str, url:str=BASE_URL)
 
  In this script, we extract by default only the first file.
  The function may be used to extract any other formats or all if of interest.
- Example usage: for single link: link = [get_archive(url)[0]]
- download_and_extract(link, path)
- For all files: links = get_archive(url)
- download_and_extract(links, path)
+ Example usage: for single link: set args.index = [value] < 3 ([0] used as default in this script)
+ For all links, just use the flag `--all` , while runing the script and comment out all lines related to wandb or set your custom params. 
 
     """
     #create file save directory if it doesn't already exits
-    output_dir.mkdir(parents=True, exist_ok=True)
+    output_dir = create_output_dir(output_dir, 
+                                   name="cell-finder-raw", 
+                                   is_datasets=True,
+                                   is_model=False)
     for link in data_links:
         archive_url = url + link
         archive_path = output_dir / link
@@ -66,11 +85,10 @@ def download_and_extract(data_links:List[str], output_dir:str, url:str=BASE_URL)
         except Exception as e:
             print(f"Error downloading or extracting {link}: {e}")
 
-
 def main():
     parser = ArgumentParser(description="Download and extract CellFinder BRAT/XML datasets.")
     parser.add_argument(
-        "--output_dir", "-o", type=str, required=True,
+        "--output_dir", "-o", type=str, default=BASE_PATH,
         help="Directory to save and extract downloaded files"
     )
     parser.add_argument(
@@ -87,6 +105,44 @@ def main():
     )
 
     args = parser.parse_args()
+
+    #wandb login
+    wandb.login(key=WANDB_TOKEN)
+
+
+    #Init wandb run
+    wandb_run = wandb.init(
+        project = "otar-3088",
+        entity = "ebi_literature",
+        job_type="dataset-acquistion",
+        dir = f"{BASE_PATH}/logs",
+        name = "cell-finder-raw",
+        tags = ["cell-finder-raw", "ner-dataset", "gold-standard", "cell-line"]
+
+
+    )
+
+    #init artifact
+    wandb_artifact = wandb.Artifact(
+        name="cell-finder-raw",
+        description="Data acquisition of raw version of cell-finder dataset from source",
+        type="dataset",
+        metadata= {
+            "data-source": "cell-finder",
+            "entity-collection-in-source-version": ["cell-line", "cell-type",
+                                                     "anatomical parts(tissues and others)", 
+                                                     "cell components",
+                                                     "genes/protein",""
+                                                     "species"],
+            "total number of sentences": 2100,
+            "total number of tokens": 65000,
+            "article_type": "full_text",
+            "total number of articles": 10, 
+            "total number of annotated entities": 5200
+            }
+
+    )
+    wandb_artifact.add_reference(args.url)
     output_dir = Path(args.output_dir)
 
     print("Fetching available archives...")
@@ -108,7 +164,9 @@ def main():
 
     print(f"\nSelected files: {selected_links}")
     download_and_extract(selected_links, output_dir, args.url)
+    wandb_run.log({"Selected Links extracted":selected_links})
+    wandb_run.log_artifact(wandb_artifact)
+    wandb_run.finish()
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == "__main__": main()

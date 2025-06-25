1877ccc3f9ccaae95617f695831974b048a3d6e6
amina-mardiyyah <amina.rufai@yahoo.com>
2025-06-25 12:41:25 +0100

    update main.py with hf integration

--- Entity-Extraction-Modular-pipeline/main.py
+++ Entity-Extraction-Modular-pipeline/main.py
@@ -1,14 +1,23 @@
+
 from pipelines.model_pipelines import flair_pipeline, hf_pipeline
+#from pipelines.model_pipelines.hf_pipeline import data_loader
 from utils.helper_functions import create_output_dir, set_seed, setup_loguru
 
 from omegaconf import DictConfig, OmegaConf
 import hydra
-import logging
+from hydra.core.hydra_config import HydraConfig
 from loguru import logger
+import logging 
 
+#flair
 from flair.trainers import ModelTrainer
 from flair.models import SequenceTagger
 from flair.trainers.plugins.loggers.wandb import WandbLogger
+
+
+#hf
+import torch 
+
 
 
 from dotenv import load_dotenv
@@ -16,95 +25,76 @@ import os
 import wandb 
 
 
-# os.environ["HYDRA_FULL_ERROR"]="1" #prints full error trace useful for debugging
+# os.environ["HYDRA_FULL_ERROR"]="1"
 
-#load wandb key
-load_dotenv()
-wandb_token = os.environ.get("WANDB_TOKEN")
+def init_wandb_run(cfg:DictConfig):
+    plain_cfg = OmegaConf.to_container(cfg, resolve=True)
+    run = wandb.init(
+        name=f"{cfg.model.name}-{wandb.util.generate_id()}",
+        config=plain_cfg,
+        job_type="Model-Training",
+        **cfg.logging.wandb
+    )
+    artifact = wandb.Artifact(
+        name=f"{cfg.model.name}-Training",
+        description=f"NER model training for {cfg.model.name} using {cfg.data.name}",
+        type="model",
+        metadata={"Model architecture": cfg.model.name, "Dataset": cfg.data.name}
+    )
+    return run, artifact
 
-#script to do: 
-"""
-1. complete hf pipeline
 
-"""
 
 @hydra.main(config_path="config", config_name="common_config", version_base=None)
+@logger.catch
 def train_model(cfg: DictConfig):
-    #login to wandb
-    wandb.login(key=wandb_token)
 
-    #init loguru logger
-    setup_loguru(cfg.logging)
+    #load environment variables
+    load_dotenv()
+    wandb_token = os.environ.get("WANDB_TOKEN")
+    BASE_PATH = os.environ.get("BASE_PATH")
 
-    logger.info(f"Current reproductiblity seed set to {cfg.seed}")
-
-    #set set for reproducibility
-    set_seed(cfg.seed) 
-
-
-    # logging.info(f"Current reproductiblity seed set to {cfg.seed}")
+    #get run model name
     model_name = cfg.model.name.lower()
 
-    # Convert Hydra config to a plain dict for wandb compatibility
-    plain_cfg = OmegaConf.to_container(cfg, resolve=True)
+    #init wandb
+    wandb.login(key=wandb_token) 
+    wandb_run, run_artifact = init_wandb_run(cfg)
 
-   
-    # Apply custom patch for wandb plugin
-    WandbLogger.attach_to = flair_pipeline.patched_attach_to
+    #init logger
+    setup_loguru(cfg.logging)
 
-    #init wandb run details and plain_config
-    wandb_run = wandb.init(
-            name=f"{model_name}-{wandb.util.generate_id()}",  # optional custom run name
-            config = plain_cfg,
-            **cfg.logging.wandb  
-    )
+    #seed reproducibility seed
+    set_seed(cfg.seed) 
+    logger.info(f"Reproducibility seed set to {cfg.seed}")
 
+    #init device
+    device = "cuda" if torch.cuda.is_available() else "cpu" #set device options["cpu", "cuda"]
 
+    logger.info("Current device set as: {device}")
+    wandb_run.log({"Current device for run" : device})
+    
+     
+
+    #init output dir for model logs and results
+    output_dir = create_output_dir(base_path=BASE_PATH, name=model_name)
+    
+
+    logger.info(f"Current Hydra output dir set at: {HydraConfig.get().runtime.output_dir}")
+    print(f"Hydra output dir: {HydraConfig.get().runtime.output_dir}")
+
+    
+    
 
     if model_name == "flair":
-        # Create model output directory ---> Future feature not yet implemented: will be used to save logs once it is setup
-        output_dir = create_output_dir(base_path ="./", model_name=model_name)
-
-        #Initialise WandbLogger plugin for flair
-        wb_plugin = WandbLogger(
-            wandb=wandb,
-            emit_alerts=True,
-            alert_level=logging.WARNING
-        )
-
-        # Create label dictionary and corpus
-        label_dict, corpus = flair_pipeline.create_label_dict_corpus(
-            label_type=cfg.model.columns[1],
-            target_column=cfg.model.columns[0],
-            data_folder=cfg.data.data_folder,
-            train_file=cfg.model.corpus.train_file,
-            dev_file=cfg.model.corpus.dev_file,
-            test_file=cfg.model.corpus.test_file,
-        )
-
-        # Load embeddings
-        embeddings = flair_pipeline.get_embeddings(cfg.model.embeddings)
-
-        # Initialize tagger
-        tagger = SequenceTagger(
-            embeddings=embeddings,
-            tag_dictionary=label_dict,
-            **cfg.model.sequence_tagger
-        )
-
-        # Train
-        trainer = ModelTrainer(tagger, corpus)
-        trainer.fine_tune(base_path=output_dir,
-                          plugins=[wb_plugin],
-                          **cfg.model.fine_tune)
-        
-        wandb_run.finish()
-
+        flair_pipeline.flair_trainer(cfg, wandb_run, run_artifact, wandb, output_dir)
     elif model_name == "hf":
-        raise NotImplementedError(f"This pipeline is yet to be implemented")
-
+        hf_pipeline.hf_trainer(cfg, wandb_run, run_artifact, output_dir, device)
     else:
-        raise ValueError(f"Unsupported model_type: {model_name}. Choose either 'flair' or 'hf'.")
+        raise ValueError(f"Unsupported model type: {model_name}. Choose 'flair' or 'hf'.")
 
-if __name__ == "__main__":
-    train_model()
+    wandb_run.finish()
+
+
+
+if __name__ == "__main__": train_model()

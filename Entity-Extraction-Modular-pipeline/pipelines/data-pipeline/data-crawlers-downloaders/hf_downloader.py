3a49ea1689f616ba1375104d4b1ccc042469aff2
amina-mardiyyah <amina.rufai@yahoo.com>
2025-07-28 14:51:12 +0100

    add new hf downloader utility script

---
+++ Entity-Extraction-Modular-pipeline/pipelines/data-pipeline/data-crawlers-downloaders/hf_downloader.py
@@ -0,0 +1,40 @@
+# """
+# This script although intended for dataset download only, 
+# it can also be used to download other files from a huggingface repository. 
+
+# """
+
+from hugging_hub import hf_hub_download
+from typing import Optional
+
+def download_hf_dataset_file(
+    repo_id: str,
+    filename: str,
+    repo_type: str = "dataset", #Set to "dataset" or "space" if downloading from a dataset or space, None or "model" if downloading from a model. Default is None
+    local_dir: str = ".",
+    revision: str = "main",
+    token: Optional[str] = None
+) -> str:
+    """
+    Download a specific file from a Hugging Face repo.
+
+    Args:
+        repo_id (str): The dataset namespace and name (e.g., "user/dataset-name").
+        filename (str): The name of the file to download (e.g., "data.conll").
+        repo_type (str): Repository type; only "dataset" is supported here.
+        local_dir (str): Local directory to save the downloaded file.
+        revision (str): Branch or tag name (default: "main").
+        token (str): Optional HF token for private datasets.
+
+    Returns:
+        str: Local path to the downloaded file.
+    """
+    local_path = hf_hub_download(
+        repo_id=repo_id,
+        filename=filename,
+        repo_type=repo_type,
+        revision=revision,
+        local_dir=local_dir,
+        token=token #not needed for public dataset
+    )
+    return local_path

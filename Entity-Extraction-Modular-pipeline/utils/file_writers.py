7869501330c70008c91f5f5d8fae4b30b68ec2be
amina-mardiyyah <amina.rufai@yahoo.com>
2025-08-11 17:54:14 +0100

    correct file imports

--- Entity-Extraction-Modular-pipeline/utils/file_writers.py
+++ Entity-Extraction-Modular-pipeline/utils/file_writers.py
@@ -1,23 +1,25 @@
-from typing import List, Dict, Optional
+from typing import List, Dict, Optional, Union
 from pathlib import Path
 
 
 
 def write_to_conll(
-    sentences: List[List[Dict[str, str]]],
+    sentences: Union[List[List[Dict[str, str]]], List[Dict[str, Union[List[str], List[str]]]]],
+    text_col: str,
+    label_col:str,
     output_path: Optional[str] = None,
     file_name: Optional[str] = None
 ) -> None:
     """
     Writes token-label pairs in CoNLL format to a file.
+    Supports both:
+        - List[List[Dict[str, str]]] with keys 'text_col(e.g "tokens")' and 'label_col(e.g "ner_tags")'
+        - List[Dict] format 
 
     Args:
-        sentences (List[List[Dict[str, str]]]):
-            A list of sentences, where each sentence is a list of dictionaries
-            with keys 'text' (token) and 'label' (IOB tag).
-        output_path (Optional[str]): Directory where the file will be saved. 
-            Defaults to 'outputs/' if not provided.
-        file_name (str): Name of the output file (without extension). Defautls to "conll_file" if not provided.
+        sentences: Input data (either token dicts or HuggingFace-style format).
+        output_path: Directory where file will be saved. Defaults to 'outputs/'.
+        file_name: Name of output file (without extension). Defaults to 'conll_file'.
 
     Returns:
         None
@@ -26,11 +28,20 @@ def write_to_conll(
     output_path.mkdir(parents=True, exist_ok=True)
 
     if not file_name:
-      file_name = "conll_file"
+        file_name = "conll_file"
     output_file = output_path / f"{file_name}.txt"
 
     with open(output_file, "w", encoding="utf-8") as f:
         for sentence in sentences:
-            for token in sentence:
-                f.write(f"{token['text']}\t{token['label']}\n")
+            # Case 1: {'words': [...], 'labels': [...]} --> HF Dataset format
+            if isinstance(sentence, dict) and text_col in sentence and label_col in sentence:
+                for word, label in zip(sentence[text_col], sentence[label_col]):
+                    f.write(f"{word}\t{label}\n")
+            # Case 2: [{'text': ..., 'label': ...}, ...] --> Other dataset annotation syle
+            elif isinstance(sentence, list) and all(isinstance(token, dict) for token in sentence):
+                for token in sentence:
+                    f.write(f"{token[text_col]}\t{token[label_col]}\n")
+            else:
+                raise ValueError("Unsupported dataset format.")
             f.write("\n")  # Sentence boundary
+

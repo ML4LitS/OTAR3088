e39b71764358f7ba84a799c9b88d07f6619e501e
amina-mardiyyah <amina.rufai@yahoo.com>
2025-08-16 07:09:45 +0100

    update  script

--- Entity-Extraction-Modular-pipeline/steps/load_ner_dataset_hf.py
+++ Entity-Extraction-Modular-pipeline/steps/load_ner_dataset_hf.py
@@ -2,10 +2,13 @@ import pandas as pd
 from typing import Tuple, List, Union
 from pathlib import Path
 
+from loguru import logger
+from omegaconf import DictConfig
 from datasets import Dataset, DatasetDict, load_dataset
 
 
 from utils.file_parsers import read_conll
+from utils.hf_utils import split_dataset
 
 
 
@@ -83,7 +86,12 @@ def _load_from_csv_tsv(file_path: str, text_col: str, label_col: str, file_type:
     if _looks_like_conll(data_lines, sep):
         return _load_from_conll(file_path, text_col, label_col)
 
-    df = pd.read_csv(file_path, sep=sep, header=0 if has_header else None)
+    try:
+        df = pd.read_csv(file_path, sep=sep, header=0 if has_header else None)
+    except pd.errors.ParserError as e:
+        new_sep = next(s for s in ["\t", ","] if s != sep)
+        print(f"Inconsistent file name <-> delimiter found, trying with '{new_sep}'")
+        df = pd.read_csv(file_path, sep=new_sep, header=0 if has_header else None)
 
     if not has_header:
         df.columns = [text_col, label_col]
@@ -104,3 +112,38 @@ def _looks_like_conll(data_lines: list[str], sep: str, threshold: float = 0.9) -
     non_empty = [line for line in data_lines if line.strip()]
     two_col_lines = [line for line in non_empty if len(line.strip().split(sep)) == 2]
     return (len(two_col_lines) / max(len(non_empty), 1)) > threshold
+
+
+
+
+def data_loader(cfg:DictConfig) -> Union[Dataset, DatasetDict]:
+
+  file_type = cfg.file_type
+  source_type = cfg.source_type
+
+  if source_type == "hf":
+    dataset = load_ner_dataset(cfg.data_folder, source_type=source_type) 
+  elif source_type == "local":
+    dataset = load_ner_dataset(cfg.data_folder, source_type=source_type, file_type=file_type)
+
+  data_split = list(dataset.keys())
+  print(data_split)
+  print(len(data_split))
+  if len(data_split) <= 1:
+    #raise ValueError(f"Dataset must have an eval set for training, but received {data_split[0]}. Use the inference pipeline if running inference")
+    # print(f"No validation set found in dataset. Auto-generating validation split using split ratio 80:20 training set")
+    logger.warning(f"No validation set found in dataset. Auto-generating validation split from training set")
+    dataset = split_dataset(dataset[data_split[0]], test_size=0.2)
+
+  train_dataset = dataset["train"]
+  eval_dataset = dataset["validation"]
+  default_column_names = train_dataset.column_names
+  
+  #rename columns for uniformity across pipeline 
+  rename_dict = {default_column_names[0]: "words",
+                 default_column_names[1]: "labels"}
+
+  train_dataset = train_dataset.rename_columns(rename_dict)
+  eval_dataset = eval_dataset.rename_columns(rename_dict)
+
+  return train_dataset, eval_dataset
